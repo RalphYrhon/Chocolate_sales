dde59c638e81d4c57c1da72531d6dc0691e15b58
Ralph <ralphcamangon09@gmail.com>
2026-02-03 16:48:46 +0800

    Update advanced

--- Advanced (Analytics & Business Insights).sql
+++ Advanced (Analytics & Business Insights).sql
@@ -1,4 +1,4 @@
--- Top 3 sales person by total amount
+-- Top 3 sales person by total amount.
 SELECT TOP 3
 	Sales_Person,
 	CAST(SUM(Amount) AS DECIMAL(10,2)) AS Amount
@@ -6,15 +6,16 @@ FROM dbo.chocolate_sales
 GROUP BY Sales_Person
 ORDER BY Amount DESC;
 
--- Ranking of countries based on total amount
+-- Ranking of countries based on total amount.
 SELECT 
 	Country,
-	CAST(SUM(Amount) AS DECIMAL(10,2)) AS Total_amount
+	CAST(SUM(Amount) AS DECIMAL(10,2)) AS Total_amount,
+    DENSE_RANK() OVER (ORDER BY CAST(SUM(Amount) AS DECIMAL(10,2)) DESC) AS [Rank]
 FROM dbo.chocolate_sales
 GROUP BY Country
-ORDER BY Total_amount;
+ORDER BY Total_amount DESC;
 
--- Best selling product per country
+-- Best selling product per country.
 WITH 
     total_sales AS (
         SELECT 
@@ -39,3 +40,26 @@ FROM total_sales t
 JOIN max_sales m
     ON t.Country = m.Country AND t.Total_sales = m.Max_sales;
 
+-- Highest single sale per product.
+SELECT 
+    [Product],
+    MAX(CAST(Amount AS DECIMAL(10,2))) AS Highest_sale
+FROM dbo.chocolate_sales
+GROUP BY [Product]
+ORDER BY Highest_sale DESC;
+
+-- Sales persons who sold more than 1 country.
+SELECT
+    Sales_Person,
+    COUNT(DISTINCT(Country)) AS Countries_sold
+FROM dbo.chocolate_sales
+GROUP BY Sales_Person
+HAVING COUNT(DISTINCT(Country)) > 1;
+
+-- Running total of amoung by date.
+SELECT
+    Date,
+    CAST(Amount AS DECIMAL(10,2)) AS Amount,
+    SUM(CAST(Amount AS DECIMAL(10,2))) OVER (ORDER BY Date ASC) AS RunningTotal
+FROM dbo.chocolate_sales
+ORDER BY Date DESC;
